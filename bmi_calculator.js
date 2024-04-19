6fe198ad395d86e78a4eeeee14b5e6cfe2068ffa
Ahmed1965 <ahmedmuhammed786@gmail.com>
2024-04-19 07:26:32 +0500

    Home Work BMI Calculator Completed

---
+++ bmi_calculator.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+console.log("\t\n\t******************** Welcome to BMI Calculator ****************\n\t");
+let bodyMass = await inquirer.prompt({
+    type: "number",
+    name: "body_mass",
+    message: "Enter your weight in kgs.....",
+});
+let height = await inquirer.prompt({
+    type: "number",
+    name: "body_height",
+    message: "Enter your height in meters.....",
+});
+console.log("\n\tYour Body Mass Index =" + " " + (bodyMass.body_mass) / (height.body_height * 2));
